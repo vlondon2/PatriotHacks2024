32e75c7ae68fc37ce34bc265d8afcbc9e77c14a4
Victor Londono <vlondon2@gmu.edu>
2024-10-11 22:20:39 -0400

    doing other shit lololololol

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [  
+    'tosapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
