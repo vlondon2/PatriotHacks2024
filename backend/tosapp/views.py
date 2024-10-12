bcff1d92b7af3dfef5e363907916386386face15
Victor Londono <vlondon2@gmu.edu>
2024-10-12 15:43:09 -0400

    get endpoints working

--- backend/tosapp/views.py
+++ backend/tosapp/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import TosDocument
 
-# Create your views here.
+# POST
+@csrf_exempt
+def summarize_tos(request):
+    text = request.POST.get('text')
+    
+    if not text:
+        return JsonResponse({"error": "No ToS text provided"}, status=400)
+    
+
+    # insert summarization logic here
+
+    # insert sorting logic here
+
+    bullets = {
+        "good": [],
+        "neutral": [],
+        "bad": []
+    }
+
+    tos_doc = TosDocument.objects.create(text=text, bullets = bullets)
+    return JsonResponse({
+        'id': tos_doc.id
+    })
+
+# GET
+def get_bullets(request):
+    try:
+        doc_id = request.GET.get('doc_id')
+        tos_doc = TosDocument.objects.get(id=doc_id)
+    except TosDocument.DoesNotExist:
+        return JsonResponse({'error': 'ToS doc not found'}, status=404)
+    
+    return JsonResponse({
+        'id': tos_doc.id,
+        'text': tos_doc.text,
+        'good': tos_doc.bullets["good"],
+        'neutral': tos_doc.bullets["neutral"],
+        'bad': tos_doc.bullets["bad"]
+
+    })
+
+
+
+
+
+
